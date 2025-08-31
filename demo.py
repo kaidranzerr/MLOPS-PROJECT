fbb78ddb4d59cdc0d0c15ebdf54985877b1ae3fe
kaidranzerr <ashstark68@gmail.com>
2025-08-31 23:36:47 +0530

    data ingestion added

--- demo.py
+++ demo.py
@@ -12,11 +12,11 @@ from src.logger import logging
 from src.exception import MyException
 import sys
 
-try:
-    a = 1+'Z'
-except Exception as e:
-    logging.info(e)
-    raise MyException(e, sys) from e
+# try:
+#     a = 1+'Z'
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys) from e
 
 # --------------------------------------------------------------------------------
 
