95e467be1f441ef9ffc6f1476781166bac2aab8c
kaidranzerr <ashstark68@gmail.com>
2025-08-31 13:51:36 +0530

    Project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="kaizer",
+    author_email="ashstark68@gmail.com",
+    packages=find_packages()
+)
